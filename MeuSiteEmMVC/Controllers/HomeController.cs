38aa0839c5169115cb6f641c0dbb795d06b0be94
Crisofer Gaier Sais <cristofer.gaier.sais@gmail.com>
2023-03-28 10:53:02 -0300

    Adicionar arquivos de projeto.

---
+++ MeuSiteEmMVC/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using MeuSiteEmMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MeuSiteEmMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+
+            home.Nome = "Cristofer Sais";
+            home.Email = "cristofer@gmail.com";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
