286bc5e9c06ac6da95d271eb1383f5ce86188fe0
Crisofer Gaier Sais <cristofer.gaier.sais@gmail.com>
2023-03-31 15:49:32 -0300

    primeira aula completa do sistema de cadastros da lista telefonica

---
+++ ControleContatos/Repositorio/ContatoRepositorio.cs
@@ -0,0 +1,28 @@
+﻿using ControleContatos.Data;
+using ControleContatos.Models;
+
+namespace ControleContatos.Repositorio
+{
+
+    //Contem os metodos para trabalhar com contatos
+    public class ContatoRepositorio : IContatoRepositorio
+    {
+        private readonly BancoContext _bancoContext;
+        public ContatoRepositorio(BancoContext bancoContext)
+        {
+            _bancoContext = bancoContext;
+        }
+
+        public List<ContatoModel> BuscarTodos()
+        {
+            return _bancoContext.Contatos.ToList();
+        }
+        public ContatoModel Adicionar(ContatoModel contato)
+        {
+            //GRAVAR NO DB
+            _bancoContext.Contatos.Add(contato);
+            _bancoContext.SaveChanges();
+            return contato;
+        }
+    }
+}
